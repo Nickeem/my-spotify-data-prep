0b53dfc55f6b7e45668992e980e2c107275ba1ef
Nickeem <vacation1478@gmail.com>
2025-05-10 21:03:56 -0400

    tested and modified classes

--- MySpotifyStats.py
+++ MySpotifyStats.py
@@ -2,10 +2,17 @@ import requests
 import os
 import pytz
 import pandas as pd
+import base64
+from datetime import datetime, timedelta, timezone
+import configparser
+from typing import Union
 
 class MySpotifyStats:
     client_id = None # Spotify API client ID
     client_secret = None # Spotify API client secret
+    client_code = None # Spotify API client code
+    streaming_history_path = None # Path to the Spotify streaming history files 
+    redirect_uri = None # Spotify API redirect URI
 
 
     def __init__(self):
@@ -17,7 +24,9 @@ class MySpotifyStats:
         # Make sure to set these environment variables in your system or IDE.
         self.client_id = os.getenv('SPOTIFY_CLIENT_ID')
         self.client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
+        self.client_code = os.getenv('SPOTIFY_CLIENT_CODE')
         self.streaming_history_path = os.getenv('SPOTIFY_STREAMING_HISTORY_PATH')
+        self.redirect_uri = os.getenv('SPOTIFY_REDIRECT_URI')
 
         if not self.client_id or not self.client_secret:
             raise Exception("Please set the SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET environment variables.")
@@ -38,24 +47,7 @@ class MySpotifyStats:
         self.streaming_history_df['ts_bb'] = self.streaming_history_df['ts'].dt.tz_convert(barbados_tz)
         self.streaming_history_df['ts_day_bb'] = self.streaming_history_df['ts_bb'].dt.date 
 
-    def generate_spotify_api_token(self):
-        # This function generates a Spotify API token using the client credentials flow.
-        url = "https://accounts.spotify.com/api/token"
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded",
-        }
-        data = {
-            "grant_type": "client_credentials",
-            "client_id": self.client_id,
-            "client_secret": self.client_secret,
-        }
-
-        response = requests.post(url, headers=headers, data=data)
-        if response.status_code == 200:
-            return response.json().get("access_token")
-        else:
-            raise Exception("Failed to generate Spotify API token. Check your credentials.")
-
+    
 
     def get_top_item(self, token, content_type, time_range='short_term', limit=5):
         # This function retrieves the user's top tracks from Spotify.
@@ -110,7 +102,7 @@ class MySpotifyStats:
 
         
 
-    def get_total_listening(self, range_start=None, range_end=None):
+    def get_total_listening(self, range_start: Union[str, datetime.date, pd.Timestamp]=None, range_end: Union[str, datetime.date, pd.Timestamp]=None) -> int:
         # Work on a copy of the DataFrame to avoid modifying the original
         df_copy = self.streaming_history_df.copy()
 
@@ -120,9 +112,9 @@ class MySpotifyStats:
             df_copy = df_copy.loc[mask]
 
         total_listening_ms = df_copy['ms_played'].sum()
-        return total_listening_ms
+        return int(total_listening_ms)
     
-    def get_total_listening_by_month(self, year=None):
+    def get_total_listening_by_month(self, year: int=None) -> dict:
         # Work on a copy of the DataFrame to avoid modifying the original
         df_copy = self.streaming_history_df.copy()
 
@@ -131,13 +123,14 @@ class MySpotifyStats:
             df_copy = df_copy[df_copy['ts_bb'].dt.year == year]
 
         # Create column for end of month of date
-        df_copy['end_of_month'] = df_copy['ts_bb'] + pd.offsets.MonthEnd(0)
+        df_copy['end_of_month'] = df_copy['ts_bb'].dt.date + pd.offsets.MonthEnd(0)
 
         # Group by end of month and sum the ms_played column
-        listening_month_df = df_copy.groupby(['end_of_month'])['ms_played'].sum().reset_index()
-        return listening_month_df
+        listening_month_df = df_copy.groupby(['end_of_month'])['ms_played'].sum()
+        listening_month_df.index = listening_month_df.index.astype(str)  # Convert the index to string format
+        return listening_month_df.to_dict()
 
-    def get_average_listening(self, range_start, range_end):
+    def get_average_listening(self, range_start: Union[str, datetime.date, pd.Timestamp]=None, range_end: Union[str, datetime.date, pd.Timestamp]=None) -> float:
         # Work on a copy of the DataFrame to avoid modifying the original
         df_copy = self.streaming_history_df.copy()
 
@@ -149,9 +142,9 @@ class MySpotifyStats:
         total_listening_ms = df_copy['ms_played'].sum()
         total_days = (df_copy['ts_day_bb'].max() - df_copy['ts_day_bb'].min()).days + 1
         average_listening_ms = total_listening_ms / total_days if total_days > 0 else 0
-        return average_listening_ms
+        return float(average_listening_ms)
 
-    def get_hour_listened(self, range_start=None, range_end=None):
+    def get_hour_listened(self, range_start: Union[str, datetime.date, pd.Timestamp]=None, range_end: Union[str, datetime.date, pd.Timestamp]=None):
         # Work on a copy of the DataFrame to avoid modifying the original
         df_copy = self.streaming_history_df.copy()
 
@@ -164,11 +157,11 @@ class MySpotifyStats:
         df_copy['hour'] = df_copy['ts_bb'].dt.hour
 
         # Group by hour and sum the ms_played column
-        listening_hour_df = df_copy.groupby(['hour'])['ms_played'].sum().reset_index()
-        return listening_hour_df
+        listening_hour_df = df_copy.groupby(['hour'])['ms_played'].sum()
+        return listening_hour_df.to_dict()
     
 
-    def get_artist_listening_time(self, artist_name, range_start=None, range_end=None):
+    def get_artist_listening_time(self, artist_name, range_start: Union[str, datetime.date, pd.Timestamp]=None, range_end: Union[str, datetime.date, pd.Timestamp]=None) -> int:
         '''
         This function retrieves the total listening time for a specific artist within a specified date range.
         
@@ -197,5 +190,118 @@ class MySpotifyStats:
         total_listening_ms = df_artist['ms_played'].sum()
         
         return total_listening_ms
+    
+
+    def btoa(self,string):
+        return base64.b64encode(string.encode('utf-8')).decode('utf-8')
+    
+    def get_config_value(self, section, key):
+        # this function gets the value of a key in a section in config.ini file
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        if config.has_option(section, key):
+            return config.get(section, key)
+        else:
+            raise Exception(f"Key '{key}' not found in section '{section}' in config.ini file.")
+        
+
+    def set_config_value(self, section, key, value):
+        # this function sets the value of a key in a section in config.ini file
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        if not config.has_section(section):
+            config.add_section(section)
+        config.set(section, key, value)
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
+
+
+    def get_access_token(self):
+        expiration = self.get_config_value('Spotify_API', 'SPOTIFY_ACCESS_TOKEN_EXPIRATION') 
+        # check if the expiration time is set and if it is in the past
+        if expiration:
+            expiration = datetime.fromisoformat(expiration).astimezone(timezone.utc)
+            if expiration > datetime.now(timezone.utc):
+                # If the token is still valid, return it
+                return self.get_config_value('Spotify_API', 'SPOTIFY_ACCESS_TOKEN')
+            else:
+                return self.refresh_spotify_api_token()
+
+        else:
+            # If the token is not set, generate a new one
+            return self.generate_spotify_api_token()
+        
+    def set_token_info(self, access_token, refresh_token, expires_in):
+        print(f"Access Token: {access_token}")
+        print(f"Refresh Token: {refresh_token}")
+        print(f"Expires In: {expires_in} seconds")
+        # This function sets the access token, refresh token, and expiration time in environment variables.
+        self.set_config_value('Spotify_API', 'SPOTIFY_ACCESS_TOKEN', access_token)
+        self.set_config_value('Spotify_API', 'SPOTIFY_REFRESH_TOKEN', refresh_token)
+        self.set_config_value('Spotify_API', 'SPOTIFY_ACCESS_TOKEN_EXPIRATION', (datetime.now(timezone.utc) + timedelta(seconds=expires_in)).isoformat())  
+
+
+    def generate_spotify_api_token(self):
+        # This function generates a Spotify API token using the client credentials flow.
+        url = "https://accounts.spotify.com/api/token"
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": f"Basic {self.btoa(self.client_id + ':' + self.client_secret)}"  ,
+        }
+        data = {
+            "code": self.client_code,
+            "redirect_uri": self.redirect_uri,  # Replace with your redirect URI
+            "grant_type": "authorization_code",
+        }
+
+        response = requests.post(url, headers=headers, data=data)
+        if response.status_code == 200:
+            response_json = response.json() # .get("access_token")
+            access_token = response_json.get("access_token")
+            refresh_token = response_json.get("refresh_token")
+            expires_in = response_json.get("expires_in") # in seconds
+            
+            # Set the access token, refresh token, and expiration time in environment variables
+            self.set_token_info(access_token, refresh_token, expires_in)
+            return access_token
+
+        else:
+            print(f"Response: {response.json()}")
+            raise Exception("Failed to generate Spotify API token. Check your credentials.")
+
+
+    def refresh_spotify_api_token(self):
+        refresh_token = self.get_config_value('Spotify_API', 'SPOTIFY_REFRESH_TOKEN') 
+        if not refresh_token:
+            raise Exception("Refresh token is not set. Please generate a new access token.")
+        # This function refreshes the Spotify API token using the refresh token.
+        url = "https://accounts.spotify.com/api/token"
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": f"Basic {self.btoa(self.client_id + ':' + self.client_secret)}",
+        }
+        data = {
+            "refresh_token": refresh_token,
+            "grant_type": "refresh_token",
+        }
+
+        response = requests.post(url, headers=headers, data=data)
+        if response.status_code == 200:
+            response_json = response.json() # .get("access_token")
+            access_token = response_json.get("access_token")
+            refresh_token = response_json.get("refresh_token")
+            expires_in = response_json.get("expires_in") # in seconds
+            
+            # There are instances depending on the grant type where the refresh token is not returned in the response and the access token is returned instead.
+            # In this case, the script can use the previously stored access token
+            if refresh_token is None and access_token is not None:
+                update_expiration = self.set_config_value('Spotify_API', 'SPOTIFY_ACCESS_TOKEN_EXPIRATION', (datetime.now(timezone.utc) + timedelta(seconds=expires_in)).isoformat())
+                return access_token
+            else:
+                # Set the access token, refresh token, and expiration time in environment variables
+                self.set_token_info(access_token, refresh_token, expires_in)
+                return access_token
+        else:
+            raise Exception("Failed to refresh Spotify API token. Check your credentials.")
 
 
--- SpotifyStatsModel.py
+++ SpotifyStatsModel.py
@@ -1,4 +1,5 @@
 from pocketbase import PocketBase
+import json
 
 class SpotifyStatsModel:
     def __init__(self, address ):
@@ -8,17 +9,38 @@ class SpotifyStatsModel:
         self.pb = PocketBase(address)
 
         # authenticate with the PocketBase server
-        user_data = self.pb.collection(self.collection_name).auth_with_password(
-            email="test@email.com",
-            password="password")
+        admin_data = self.pb.admins.auth_with_password(
+            email="daily@drive.app",
+            password="HPaqcFihMZoyNtX", )
         
-        if not user_data.is_valid:
+        if not admin_data.is_valid:
             raise Exception("Failed to authenticate with PocketBase server. Check your credentials.")
         
-        self.user_data = user_data
+        self.admin_data = admin_data
 
 
-    def update_total_minutes(self, total_minutes):
+    def update_stats(self, record_id, label, value):
+        '''
+        This function updates value of a stat
+        
+        Parameters:
+        record_id (str): The record ID of the stat to update.
+        value (int): The new value to set for the stat.
+        
+        Returns:
+        None
+        '''
+        
+        # Update the total minutes listened to in the PocketBase database
+        self.pb.collection(self.collection_name).update(
+
+            record_id, # total_ms_listened record id
+            {
+                "value": json.dumps({label:value})
+            }
+        )
+    
+    def update_total_ms_listened(self, total_ms_listened: int):
         '''
         This function updates the total minutes listened to in the PocketBase database.
         
@@ -27,12 +49,104 @@ class SpotifyStatsModel:
         
         Returns:
         None
-        '''
-        
+        ''' 
         # Update the total minutes listened to in the PocketBase database
-        self.pb.collection(self.collection_name).update(
-            self.user_data.record.id,
-            {
-                "total_minutes": total_minutes
-            }
-        )
+        total_ms_listened_record_id = 'k79p34vx2t81ppm' # total_ms_listened record id
+        label = 'total_ms_listened'
+        self.update_stats(total_ms_listened_record_id, label, total_ms_listened)
+
+    
+    def update_total_ms_listened_last_year(self, total_ms_listened_last_year):
+        '''
+        This function updates the total minutes listened to last year in the PocketBase database.
+        
+        Parameters:
+        total_ms_listened_last_year (int): The total minutes listened to.
+        
+        Returns:
+        None
+        ''' 
+        # Update the total minutes listened to in the PocketBase database
+        total_ms_listened_ly_record_id = 'g1u3v64la2ufpyx'
+        label = 'total_ms_listened_last_year'
+        self.update_stats(total_ms_listened_ly_record_id, label, total_ms_listened_last_year)
+
+    def update_average_ms_listened(self, average_ms_listened):
+        '''
+        This function updates the average minutes listened to in the PocketBase database.
+        
+        Parameters:
+        average_ms_listened (int): The average minutes listened to.
+        
+        Returns:
+        None
+        ''' 
+        # Update the total minutes listened to in the PocketBase database
+        average_ms_listened_record_id = 'cvcl26dhoq3698a'
+        label = 'average_ms_listened'
+        self.update_stats(average_ms_listened_record_id, label, average_ms_listened)
+
+    def update_top_5_songs_recent(self, top_5_songs_recent):
+        '''
+        This function updates the top 5 songs listened to in the PocketBase database.
+        
+        Parameters:
+        top_5_songs_recent (list): The list of top 5 songs listened to.
+        
+        Returns:
+        None
+        ''' 
+        # Update the total minutes listened to in the PocketBase database
+        top_5_songs_recent_record_id = 'ncdzc3y1paeayj4'
+        label = 'top_5_songs_recent'
+        self.update_stats(top_5_songs_recent_record_id, label, top_5_songs_recent)
+
+    def update_top_5_artists_recent(self, top_5_artists_recent):
+        '''
+        This function updates the top 5 artists listened to in the PocketBase database.
+        
+        Parameters:
+        top_5_artists_recent (list): The list of top 5 artists listened to.
+        
+        Returns:
+        None
+        ''' 
+        # Update the total minutes listened to in the PocketBase database
+        top_5_artists_recent_record_id = '3726130j114pzzm'
+        label = 'top_5_artists_recent'
+        self.update_stats(top_5_artists_recent_record_id, label, top_5_artists_recent)
+
+    def update_mom_ly_ms_listened(self, mom_ms_listened):
+        '''
+        This function updates the minutes listened to in the PocketBase database.
+        
+        Parameters:
+        mom_ms_listened (int): The minutes listened to.
+        
+        Returns:
+        None
+        ''' 
+        # Update the total minutes listened to in the PocketBase database
+        mom_ms_listened_record_id = '723y18jg6z2srvx'
+        label = 'mom_ly_ms_listened'
+        self.update_stats(mom_ms_listened_record_id, label, mom_ms_listened)
+
+    def update_listening_clock_ly(self, listening_clock_ly):
+        '''
+        This function updates the listening clock in the PocketBase database.
+        
+        Parameters:
+        listening_clock_ly (list): The list of listening clock.
+        
+        Returns:
+        None
+        ''' 
+        # Update the total minutes listened to in the PocketBase database
+        listening_clock_ly_record_id = '7pc06xg3ix8c847'
+        label = 'listening_clock_ly'
+        self.update_stats(listening_clock_ly_record_id, label, listening_clock_ly)
+
+    
+        
+
+    
