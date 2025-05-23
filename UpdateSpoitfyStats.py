ad45df1f8d946964f52218862550484c19150d9f
Nickeem <vacation1478@gmail.com>
2025-05-22 20:46:21 -0400

    updated and tested controller

--- UpdateSpoitfyStats.py
+++ UpdateSpoitfyStats.py
@@ -2,9 +2,76 @@ from MySpotifyStats import MySpotifyStats
 from SpotifyStatsModel import SpotifyStatsModel
 import datetime as dt
 
+def simplify_top_songs(top_songs):
+    top_songs_simplified = []
+    for track in top_songs:
+        # artist name
+        artists = ', '.join(artist['name'] for artist in track['artists'])
+        # track name
+        track_name = track['name']
+        # track id
+        track_id = track['id']
+        # track link
+        track_link = track['external_urls']['spotify']
+        # track image 640 x 640
+        track_image_640 = track['album']['images'][0]['url']
+        # track image 300 x 300
+        track_image_300 = track['album']['images'][1]['url']
+        # track image 64 x 64
+        track_image_64 = track['album']['images'][2]['url']
+        # track release date
+        track_release_date = track['album']['release_date']
+        top_songs_simplified.append({
+            'artists': artists,
+            'track_name': track_name,
+            'track_id': track_id,
+            'track_link': track_link,
+            'track_image_640': track_image_640,
+            'track_image_300': track_image_300,
+            'track_image_64': track_image_64,
+            'track_release_date': track_release_date
+        })
+    return top_songs_simplified 
+
+def simplify_top_artists(top_artists):
+    top_artists_simplified = []
+    for artist in top_artists:
+        # artist name
+        artist_name = artist['name']
+        # artist id
+        artist_id = artist['id']
+        # artist link
+        artist_link = artist['external_urls']['spotify']
+        #artist genres
+        artist_genres = ', '.join(artist['genres'])
+        # artist popularity
+        artist_popularity = artist['popularity']
+        # artist followers
+        artist_followers = artist['followers']['total']
+        # artist image 640 x 640
+        artist_image_640 = artist['images'][0]['url']
+        # artist image 300 x 300
+        artist_image_300 = artist['images'][1]['url']
+        # artist image 64 x 64
+        artist_image_64 = artist['images'][2]['url']
+        top_artists_simplified.append({
+            'artist_name': artist_name,
+            'artist_id': artist_id,
+            'artist_link': artist_link,
+            'artist_genres': artist_genres,
+            'artist_popularity': artist_popularity,
+            'artist_followers': artist_followers,
+            'artist_image_640': artist_image_640,
+            'artist_image_300': artist_image_300,
+            'artist_image_64': artist_image_64
+        })
+    return top_artists_simplified
+
+
 
 stats = MySpotifyStats()
 stats_model = SpotifyStatsModel("http://192.168.1.130:8090")
+
 
 # get dates for beginning of last year and end of last year
 start_date = dt.date(dt.datetime.now().year - 1, 1, 1)
@@ -25,11 +92,13 @@ average_ms_listened = stats.get_average_listening(start_date, end_date)
 stats_model.update_average_ms_listened(average_ms_listened)
 
 # update top 5 songs listened to
-top_5_songs_recent = stats.get_top_tracks(token)
+top_5_songs_recent_raw = stats.get_top_tracks(token)
+top_5_songs_recent = simplify_top_songs(top_5_songs_recent_raw)
 stats_model.update_top_5_songs_recent(top_5_songs_recent)
 
 # update top 5 artists listened to
-top_5_artists_recent = stats.get_top_artists(token)
+top_5_artists_recent_raw = stats.get_top_artists(token)
+top_5_artists_recent = simplify_top_artists(top_5_artists_recent_raw)
 stats_model.update_top_5_artists_recent(top_5_artists_recent)
 
 # # update month over month listening stats
